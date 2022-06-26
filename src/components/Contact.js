816ffd3cbe48d4744a5734755784b3af8b160b77
Rio Alvarez <yoalvarez5@gmail.com>
2022-06-26 11:16:45 +0700

    commit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,20 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
-import React from 'react';
-
-const Contact = () => {
-    return (
-        <></>
-    )
-}
+import React from "react";
+import "./Contact.css";
+const Contact = ({ data }) => {
+  return (
+    <div className="contact">
+      <div className="foto">
+        <img src={data.photo} alt={data.name} />
+      </div>
+      <div className="data">
+        <h2>{data.name}</h2>
+        <h3>{data.phone}</h3>
+        <h4>{data.email}</h4>
+      </div>
+    </div>
+  );
+};
 
 export default Contact;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,26 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
-import React from 'react';
-
+import React from "react";
+import "./Header.css";
 const Header = () => {
-    return (
-        <></>
-    )
-}
+  return (
+    <div className="header">
+      <div className="isi">
+        <h1 className="header-title">Call a Friend</h1>
+        <p className="header-subtitle">your friendly contact app</p>
+        <p className="header-dot">
+          &#8226; &#8226; &#8226; &#8226; &#8226; &#8226; &#8226; &#8226;
+          &#8226; &#8226; &#8226; &#8226; &#8226; &#8226; &#8226; &#8226;
+          &#8226; &#8226; &#8226; &#8226; &#8226; &#8226; &#8226; &#8226;
+          &#8226; &#8226; &#8226; &#8226; &#8226; &#8226; &#8226; &#8226;
+          &#8226; &#8226; &#8226; &#8226; &#8226; &#8226; &#8226; &#8226;
+          &#8226; &#8226; &#8226; &#8226; &#8226; &#8226; &#8226; &#8226;
+          &#8226; &#8226; &#8226; &#8226; &#8226; &#8226; &#8226; &#8226;
+          &#8226; &#8226; &#8226; &#8226; &#8226; &#8226; &#8226; &#8226;
+          &#8226; &#8226; &#8226; &#8226; &#8226; &#8226; &#8226; &#8226;
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
